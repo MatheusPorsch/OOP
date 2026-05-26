e4522bc7584aa3e82241221f3372b1ad8ae9076c
Eduardo da Costa <eduardoreiscosta2007@gmail.com>
2026-05-25 22:55:32 -0300

    Update Tarefa.java

--- Tarefa.java
+++ Tarefa.java
@@ -1,4 +1,43 @@
-
 public class Tarefa {
+    
+    private int codigo;
+    private String nome;
+    private String descricao;
+    
+    public Tarefa(int codigo, String nome, String descricao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
+    @Override
+    public String toString() {
+        return "Tarefa [codigo=" + codigo +
+               ", nome=" + nome +
+               ", descricao=" + descricao + "]";
+    }
 }
